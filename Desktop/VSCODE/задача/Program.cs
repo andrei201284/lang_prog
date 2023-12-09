35416b0ce4bb9ba487705fd519d99768e9bee438
andrei201284 <148987410+andrei201284@users.noreply.github.com>
2023-12-09 17:21:25 +0900

    ЗАДАЧА1

---
+++ Desktop/VSCODE/задача/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает два
+ целых числа и проверяет является ли первое число квадратом второго.
+25 5 -> YES
+10 4 -> NO
+16 4 -> YES
+*/
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+    Console.Write("YES");
+else
+    Console.Write("NO");
+        
